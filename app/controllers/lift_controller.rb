1b45cfd984feb4ea0283ae366c1aa12afbda4f76
Jonathan Maddison <jmaddi@gmail.com>
2013-03-11 14:18:08 +1100

    reverted error handling

--- app/controllers/lift_controller.rb
+++ app/controllers/lift_controller.rb
@@ -1,15 +1,15 @@
 class LiftController < ApplicationController
   def login
-    #begin
-    client = Liftapp::Client.new(params[:email], params[:password])
-    user = User.where(email: client.email).first_or_create(lift_password: params[:password], full_name: client.name, picture_url: client.picture_url)
-    sign_in :user, user
-    flash[:notice] = 'You have been logged in successfully.'
-    #rescue Liftapp::AccessDenied
-    #  flash[:error] = 'Invalid email/password'
-    #rescue
-    #  flash[:error] = 'Error logging in, please try again'
-    #end
+    begin
+      client = Liftapp::Client.new(params[:email], params[:password])
+      user = User.where(email: client.email).first_or_create(lift_password: params[:password], full_name: client.name, picture_url: client.picture_url)
+      sign_in :user, user
+      flash[:notice] = 'You have been logged in successfully.'
+    rescue Liftapp::AccessDenied
+      flash[:error] = 'Invalid email/password'
+    rescue
+      flash[:error] = 'Error logging in, please try again'
+    end
     redirect_to root_path
   end
   def habits
